d99f021dbdf57a91ad1760f8d08d1eb4da710050
peanutoil <harinib246@gmail.com>
2025-03-13 16:48:22 -0400

    stutterify function

--- discordify/discordify.py
+++ discordify/discordify.py
@@ -1,3 +1,5 @@
+import re
+
 def dummify(s):
     vowels = "aeiouAEIOU"
     vowel_list = [char for char in s if char in vowels]
@@ -8,3 +10,29 @@ def dummify(s):
         else:
             result.append(char)
     return "".join(result)
+
+def stutterify(s):
+    pronouns = {"you","wait","what","so","um","uh","i"}
+    conjunctions = {'but','and'}
+
+    def stutter(word):
+        prefix = word[:1]
+        return f"{prefix}-{word}" if word else word
+    
+    words = re.findall(r"\b\w+\b|[^\w\s]", text)
+    if words:
+        words[0] = stutter(words[0])
+    for i in range(1, len(words)):
+        word = words[i].lower()
+        if word in pronouns:
+            words[i] = stutter(words[i])
+        elif word in conjunctions:
+            words[i] += "..."  
+    result = []
+    for i, word in enumerate(words):
+        if i > 0 and re.match(r"[.,!?;:]", word):  
+            result[-1] += word
+        else:
+            result.append(word)
+
+    return " ".join(result)
